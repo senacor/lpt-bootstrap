565acf7c35cb27771d3eaec4787a603ecc6e4445
patrick.siegel <patrick.siegel@senacor.com>
2024-10-29 16:05:57 +0100

    Remove unknown instruction

--- scopes/application/04_webapp_bucket.tf
+++ scopes/application/04_webapp_bucket.tf
@@ -6,7 +6,6 @@ resource "google_storage_bucket" "lpt_ui" {
   name     = var.ui_bucket_name
   location = var.gcp_region
   project = data.google_project.lpt_proj.project_id
-  public_access_prevention = "false"
 
   website {
     main_page_suffix = "index.html"
