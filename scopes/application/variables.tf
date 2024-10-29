b0aaadc7fa291f332faa6278a4f9ae4343501146
patrick.siegel <patrick.siegel@senacor.com>
2024-10-29 15:00:26 +0100

    Add cloud storage bucket for "website deployment"

--- scopes/application/variables.tf
+++ scopes/application/variables.tf
@@ -1,7 +1,4 @@
-variable "github_token" {
-  description = "Github token to access and change ressources through the API"
-  type        = string
-}
+
 
 variable "project_id" {
   description = "Project ID to apply and identify infrastructure code"
@@ -12,4 +9,10 @@ variable "gcp_region" {
   description = "Region the infrastructure should be deployed in"
   type        = string
   default     = "europe-west1"
+}
+
+variable "ui_bucket_name" {
+  description = "Name of cloud storage bucket for UI hosting"
+  type        = string
+  default     = "lpt-ui"
 }
